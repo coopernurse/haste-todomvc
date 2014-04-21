e3054dc8b0f53065aaa83c9db7bcac7d65a44a40
James Cooper <jamespcooper@gmail.com>
2014-04-20 21:06:08 -0700

    add 'watch' target.  modify app to log new todo when enter pressed.

--- Makefile
+++ Makefile
@@ -1,2 +1,6 @@
 todo.js: todo.hs
 	hastec -Wall -fno-warn-unused-do-bind todo.hs
+
+# to install wr:  npm install -g wr
+watch:
+	wr --exec make *.hs 
--- todo.hs
+++ todo.hs
@@ -1,8 +1,24 @@
 module Main where
 
 import Haste
+import qualified Control.Concurrent as C
+import Data.List
 
 main = withElems ["new-todo", "todo-list", "toggle-all"] start
 
+addTodo todos newTD = do
+	cur <- C.takeMVar todos
+	C.putMVar todos $ cur++[newTD]
+	cur' <- C.readMVar todos
+	writeLog $ "NewTD: " ++ (intercalate ", " cur')
+
 start [newTodo, todoList, toggleAll] = do
-	setProp newTodo "innerHTML" "something"
+	todos <- C.newMVar []
+	onEvent newTodo OnKeyUp $ \k -> do   -- TODO: clean this up. how do we filter events?
+		case k of
+			13 -> do
+				newTD <- getProp newTodo "value"
+				addTodo todos newTD
+				setProp newTodo "value" ""
+			_ -> return ()
+		
