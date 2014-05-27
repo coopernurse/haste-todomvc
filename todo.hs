c4e2e58e19ebdab5722429c3161ae81c3c0d7bc4
James Cooper <jamespcooper@gmail.com>
2014-05-26 17:21:32 -0700

    integrate IORef to store todo list state

--- todo.hs
+++ todo.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Data.IORef
 import Haste
 import Haste.DOM
 import View 
@@ -7,12 +8,15 @@ import Model
 
 main = withElems ["new-todo", "todo-list", "toggle-all"] start
 
-onTodoKeyUp input todoList k = case k of
+onTodoKeyUp input todoList tsRef k = case k of
   13 -> do
     Just v <- getValue input
-    setProp todoList "innerHTML" $ renderTodos $ addTodo [] v
+    modifyIORef tsRef (\ts -> addTodo ts v)
+    ts <- readIORef tsRef
+    setProp todoList "innerHTML" $ renderTodos $ ts
     setProp input "value" ""
   _  -> return ()
 
 start [todoInput, todoList, toggleAll] = do
-  onEvent todoInput OnKeyUp (onTodoKeyUp todoInput todoList)
+  tsRef <- newIORef $ []
+  onEvent todoInput OnKeyUp (onTodoKeyUp todoInput todoList tsRef)
