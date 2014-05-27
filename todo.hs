d0aba46a9baad912224ba0d8ceabfc59412a798d
James Cooper <jamespcooper@gmail.com>
2014-05-26 17:31:15 -0700

    ignore empty string todos

--- todo.hs
+++ todo.hs
@@ -11,10 +11,13 @@ main = withElems ["new-todo", "todo-list", "toggle-all"] start
 onTodoKeyUp input todoList tsRef k = case k of
   13 -> do
     Just v <- getValue input
-    modifyIORef tsRef (\ts -> addTodo ts v)
-    ts <- readIORef tsRef
-    setProp todoList "innerHTML" $ renderTodos $ ts
-    setProp input "value" ""
+    if v == "" then
+      return ()
+    else do
+      modifyIORef tsRef (\ts -> addTodo ts v)
+      ts <- readIORef tsRef
+      setProp todoList "innerHTML" $ renderTodos $ ts
+      setProp input "value" ""
   _  -> return ()
 
 start [todoInput, todoList, toggleAll] = do
